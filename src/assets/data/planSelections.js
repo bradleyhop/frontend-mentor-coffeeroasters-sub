2b19163394199f84a24d2639b96071220e28628c
Bradley Smith <bradleyhop@gmail.com>
2023-10-20 14:49:53 -0400

    build details/summary skeleton

---
+++ src/assets/data/planSelections.js
@@ -0,0 +1,104 @@
+const planSelections = [
+  {
+    selectionTitle: 'How do you drink your coffee?',
+    selections: [
+      {
+        selectionName: 'Capsule',
+        selectionDescription:
+          'Compatible with Nespresso systems and similar brewers',
+      },
+      {
+        selectionName: 'Filter',
+        selectionDescription:
+          'For pour over or drip methods like Aeropress, Chemex, and V60',
+      },
+      {
+        selectionName: 'Espresso',
+        selectionDescription:
+          'Dense and finely ground beans for an intense, flavorful experience',
+      },
+    ],
+  },
+  {
+    selectionTitle: 'What type of coffee?',
+    selections: [
+      {
+        selectionName: 'Single Origin',
+        selectionDescription:
+          'Distinct, high quality coffee from a specific family-owned farm',
+      },
+      {
+        selectionName: 'Decaf',
+        selectionDescription:
+          'Just like regular coffee, except the caffeine has been removed',
+      },
+      {
+        selectionName: 'Blended',
+        selectionDescription:
+          'Combination of two or three dark roasted beans of organic coffees',
+      },
+    ],
+  },
+  {
+    selectionTitle: 'How much would you like?',
+    selections: [
+      {
+        selectionName: '250g',
+        selectionDescription:
+          'Perfect for the solo drinker. Yields about 12 delicious cups.',
+      },
+      {
+        selectionName: '500g',
+        selectionDescription:
+          'Perfect option for a couple. Yields about 40 delectable cups.',
+      },
+      {
+        selectionName: '1000g',
+        selectionDescription:
+          'Perfect for offices and events. Yields about 90 delightful cups.',
+      },
+    ],
+  },
+  {
+    selectionTitle: 'Want us to grind them?',
+    selections: [
+      {
+        selectionName: 'Wholebean',
+        selectionDescription:
+          'Best choice if you cherish the full sensory experience',
+      },
+      {
+        selectionName: 'Filter',
+        selectionDescription:
+          'For drip or pour-over coffee methods such as V60 or Aeropress',
+      },
+      {
+        selectionName: 'Cafetiére',
+        selectionDescription:
+          'Course ground beans specially suited for french press coffee',
+      },
+    ],
+  },
+  {
+    selectionTitle: 'How often should we deliver?',
+    selections: [
+      {
+        selectionName: 'Every week',
+        selectionDescription:
+          '$14.00 per shipment. Includes free first-class shipping.',
+      },
+      {
+        selectionName: 'Every 2 weeks',
+        selectionDescription:
+          '$17.25 per shipment. Includes free priority shipping.',
+      },
+      {
+        selectionName: 'Every 3 weeks',
+        selectionDescription:
+          '$22.50 per shipment. Includes free priority shipping.',
+      },
+    ],
+  },
+];
+
+export default planSelections;
